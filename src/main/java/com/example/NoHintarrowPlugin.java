ba073cfbf5b1294dd66f57169f59d543cb6b5c2f
JNCressey <42148702+JNCressey@users.noreply.github.com>
2025-12-17 03:13:55 +0000

    Initial configuration setup to step 9 of guide: https://github.com/runelite/plugin-hub

--- src/main/java/com/example/NoHintarrowPlugin.java
+++ src/main/java/com/example/NoHintarrowPlugin.java
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "No Hint-arrow"
 )
-public class ExamplePlugin extends Plugin
+public class NoHintarrowPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private NoHintarrowConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	NoHintarrowConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(NoHintarrowConfig.class);
 	}
 }
